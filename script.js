02752f80681ce347f602f1f82caf02b3de5372e5
Rajdeep Seal <rajdeep.pbsb@gmail.com>
2024-03-29 18:21:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const btnsOpenModal = document.querySelector(".rule");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+
+
+btnsOpenModal.addEventListener("click", function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+});
+btnCloseModal.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
+overlay.addEventListener("click", function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        kt:['Gill Sans'],
+        'kt-cursive':['cursive'],
+        'kt-fantasy':['fantasy'],
+      }
+    },
+  },
+  plugins: [],
+}
+
