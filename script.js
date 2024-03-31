ed58a8ce50eac61c405493efb0f802574a486a29
Rajdeep Seal <rajdeep.pbsb@gmail.com>
2024-03-31 12:51:00 +0530

    update

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 'use strict';
-
+//selecting elements
+const player0 = document.querySelector('.player--0')
+const player1 = document.querySelector('.player--1')
+const score0 = document.getElementById('score--0')
+const score1 = document.getElementById('score--1')
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
+const current0 = document.getElementById("current--0");
+const current1 = document.getElementById("current--1");
 const btnsOpenModal = document.querySelector(".rule");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
+const winMessage = document.querySelector('.player--wins')
+const diceEl = document.querySelector(".dice");
 
 
 btnsOpenModal.addEventListener("click", function () {
